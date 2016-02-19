aa3e518f570418c195ad4178f352dd250714133f
davidandrus <davidandrus.com@gmail.com>
2016-02-19 07:35:18 -0800

    fixing eslint config

--- _src/reducers/rootReducer.test.js
+++
@@ -1,7 +0,0 @@
-import test from 'tape';
-
-test('Should reduce yo', assert => {
-  console.log({cool : 'bro'})
-  assert.equal(true, true);
-  assert.end();
-});
--- src/components/Counter.test.jsx
+++ src/components/Counter.test.jsx
@@ -2,13 +2,6 @@ import React from 'react';
 import test from 'tape';
 import { shallow } from 'enzyme';
 import Counter from './Counter';
-//
-// test('should set disabled when prop is disabled', assert => {
-//   const subject = shallow(<Button disabled="true">Check it</Button>);
-//   assert.ok(subject.props().disabled);
-//   assert.end();
-// });
-
 
 function getCounter(props = {}) {
   const NOOP = Function.prototype;
@@ -17,15 +10,14 @@ function getCounter(props = {}) {
     incrementIfOdd: NOOP,
     decrement: NOOP,
     counter: 0,
-    ...props
+    ...props,
   };
 
   return shallow(<Counter {...PROPS} />);
 }
 
-test("it should should show count", assert => {
-  const subject = getCounter({counter : 2});
-  //assert.equal(subject.)
+test('it should should show count', assert => {
+  const subject = getCounter({ counter: 2 });
   assert.equal(subject.find('span').text(), '2');
   assert.end();
-})
+});
