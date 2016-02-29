f280b8b4b6465074e1c1e88ce81fb1ded1e8eb1a
davidandrus <davidandrus.com@gmail.com>
2016-02-28 19:43:11 -0800

    checking in progress

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,19 +1,38 @@
 import React, { Component, PropTypes } from 'react';
+import styles from './Counter.style';
+
+console.log(styles);
 
 export default class Counter extends Component {
 
   render() {
     const { increment, incrementIfOdd, decrement, counter } = this.props;
     return (
-      <p>
-        Clicked: <span>{counter}</span> times
-        {' '}
-        <button onClick={increment}>+</button>
-        {' '}
-        <button onClick={decrement}>-</button>
-        {' '}
-        <button onClick={incrementIfOdd}>Increment if odd sucka</button>
-      </p>
+      <div style={styles.wrapper}>
+        <p style={styles.message}>
+          Clicked: <span>{counter}</span> times
+        </p>
+        <div style={styles.buttonGroup}>
+          <button
+            style={styles.buttonIncrement}
+            onClick={increment}
+          >
+            +
+          </button>
+          <button
+            style={styles.buttonDecrement}
+            onClick={decrement}
+          >
+            -
+          </button>
+          <button
+            style={styles.buttonIncrementIfOdd}
+            onClick={incrementIfOdd}
+          >
+            Increment if odd sucka
+          </button>
+        </div>
+      </div>
     );
   }
 }
---
+++ src/components/Counter.style.js
@@ -0,0 +1,42 @@
+const BUTTON_STYLE = {
+  border: '1px solid red',
+  backgroundColor: 'gray',
+  boxShadow: 'none',
+  marginRight: '20px',
+};
+
+const STYLES = {
+  wrapper: {
+    backgroundColor: 'blue',
+    border: '1px solid green',
+    display: 'inline-flex',
+    flexDirection: 'column',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    maxWidth: '300px',
+    padding: '20px',
+    justifyContent: 'space-around',
+    textAlign: 'center',
+  },
+  message: {
+    display: 'block',
+    fontSize : '30px',
+    width: '100%',
+  },
+  buttonGroup: {
+    display: 'flex',
+    width: '100%',
+  },
+  buttonIncrement: {
+    ...BUTTON_STYLE,
+  },
+  buttonDecrement: {
+    ...BUTTON_STYLE,
+  },
+  buttonIncrementIfOdd: {
+    ...BUTTON_STYLE,
+    marginRight: 0,
+  },
+};
+
+export default STYLES;
